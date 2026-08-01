f50764149f847d45dd738c822d013769fe80fc83
jesse-rb <jesse.reynekebarnard@gmail.com>
2026-08-01 12:58:00 +1200

    wip

--- src/services/firebase/service.ts
+++ src/services/firebase/service.ts
@@ -1,15 +1,34 @@
 import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth, type Auth } from "firebase/auth";
+import { initializeUI, type FirebaseUIStore } from "@firebase-oss/ui-core";
 
-const firebaseService = () => {
-    const config = {
+class FirebaseService {
+    protected config = {
         apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
         authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
         projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
         messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-        appId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
     };
+    protected app = initializeApp(this.config);
+    protected auth = getAuth(this.app);
+    protected ui = initializeUI({
+        app: this.app,
+    });
 
-    const app = initializeApp(config);
-};
+    constructor() {
+        if (import.meta.env.VITE_ENV === "local") {
+            connectAuthEmulator(this.auth, "http://localhost:9099");
+        }
+    }
 
-export default firebaseService;
+    getUI(): FirebaseUIStore {
+        return this.ui;
+    }
+
+    getAuth(): Auth {
+        return this.auth;
+    }
+}
+
+export default new FirebaseService();
